2bf7e86efafe66571d8052dcaba13b4ec09228d9
Haizard <haithammisape@gmail.com>
2023-09-19 04:59:59 -0700

    adding dropdown menu on hover to the nav link "explore"

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,21 +1,59 @@
 import React from 'react'
 import './navbar.css'
+
+
+function MenuItems() {
+  const MenuList = ["hi haitham", "hi haitham slehe", "haitham misape", "hi haizard"];
+  return (
+    <>
+   <ul>
+    <li>
+      {MenuList.map((item)=> (
+      <li className='list' key={item.id}>{item}</li>
+      ))}
+     
+     
+    </li>
+   </ul>
+    </>  
+  )
+}
+
+
+
 function Navbar() {
   return (
-    <div className='navbar'>
-     <p className='text'>List, Find, or Review Business in <br /> Marietta Georgia</p>
-     <p className="text1">List your businessin our directory for free or explore local attraction</p>
-     <div className="inputsection">
-      <input type="text" className='input' placeholder='what are you looking for'/>
-     
-      <div className='location'>dar es salaam</div>
-      
-      <button className="option" >
+    <>
+     <div className='header'>
+  
+          <div className='logo'>MariettaGoergia.org</div>
+          <div className='header-list'>
+            <p><a href='#add your business'>add your business</a></p>
 
-      </button>
-      <input className='input' type="search" name="search" id="search-box" placeholder='search'/>
+            <div className="dropdown">
+
+              <p className='dropdown-button'><a href='#explore'>explore</a></p>
+
+              <div class="dropdown-menu">       
+                <a href="#navbar">restaurant</a>
+                <a href="#navbar">art & entertaiment</a>
+                <a href="#navbar">nature trail</a>
+                <a href="#navbar">find a hotel in marietta goergia</a>
+                <a href="#navbar">short-term rentals in Marietta goergia </a>
+                <a href="#navbar">about marrieta goergia</a>
+
+
+
+              </div>
+           
+            </div>
+
+            <p onClick = {MenuItems}><a href='#foreclosure on marietta goergia'>foreclosure on marietta goergia</a></p>
+            <p><a href='#message'>message</a></p>
+            <p><a href='#login'>login</a></p>
+          </div>
      </div>
-    </div>
+    </>
   )
 }
 
