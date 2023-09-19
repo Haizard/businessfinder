241cefde0b6c6addb1705db22cf22f12da99b4ea
Haizard <haithammisape@gmail.com>
2023-09-19 05:04:37 -0700

    no changees

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import './app.css'
-import { BlogT, BussFinder, Gallery, Header, Hotel, MoneyList, Navbar} from './components';
+import { BlogT, BussFinder, Gallery, Hotel, MoneyList, Header, Navbar } from './components';
 const App = () => {
   return (
     <div>
       <div className='app'>
-      <Header />
       <Navbar />
+      <Header />
       <BlogT />
       <BussFinder />
       <Gallery />
