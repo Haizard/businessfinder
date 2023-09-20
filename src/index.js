0d6bf56fce5097038caa381c65d05e1a1c006be2
Haizard <haithammisape@gmail.com>
2023-09-20 10:28:46 -0700

    update the index.file

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
-import ReactDOM  from "react-dom";
-import './app.css'
-
+import ReactDOM from "react-dom";
+import './app.css';
 import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById("root")); // Replace 'root' with your target DOM element
+root.render(<App />);
