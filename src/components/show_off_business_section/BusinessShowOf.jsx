018eb6ca3c1310a532052159e9d4b9682ffb073d
Haizard <haithammisape@gmail.com>
2023-09-27 14:00:01 -0700

    its a new component for business show off

---
+++ src/components/show_off_business_section/BusinessShowOf.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './businessshowof.css'
+
+function BusinessShowOf() {
+  return (
+    <div className='business_show_off_section'>
+        <span className="business_show_off_title">Get Business Exposure</span>
+        <div className="business_show_off_content">
+            <p className="business_show_off_text">Close your eyes and transport yourself to an era when the United States was deeply divided, marred by racial segregation, discrimination, and violence. In the midst of this turbulent period, a man named Malcolm Little emerged into the world on May 19, 1925, in Omaha, Nebraska.</p>
+        </div>
+    </div>
+  )
+}
+
+export default BusinessShowOf
