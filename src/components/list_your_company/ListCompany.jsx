8316d1c860084591423d6e715a376ddb619421a6
Haizard <haithammisape@gmail.com>
2023-09-28 02:26:32 -0700

    modification appled to list company section

--- src/components/list_your_company/ListCompany.jsx
+++ src/components/list_your_company/ListCompany.jsx
@@ -4,12 +4,14 @@ import './listcompany.css'
 function ListCompany() {
   return (
     <div className='list_company_section'>
-      <div className="list_company_content">
-        <span className="company_section_title">free exposure for business in marrieta goergia</span>
-        <p className="list_company_text">Close your eyes and transport yourself to discrimination, and violence. In the midst of this turbulent period, a man named Malcolm Little emerged into the world on May 19, 1925, in Omaha, Nebraska.</p>
+      <div className="list_content_container">
+        <div className="list_company_content">
+          <span className="company_section_title">free exposure for business in marrieta goergia</span>
+          <p className="list_company_text">Close your eyes and transport yourself to discrimination, and violence. In the midst of this turbulent period, a man named Malcolm Little emerged into the world on May 19, 1925, in Omaha, Nebraska.</p>
+        </div>
+        <div className="list_company_action">
+        <button className='list_company_button'>List Your Company</button>
       </div>
-      <div className="list_company_action">
-      <button className='list_company_button'>List Your Company</button>
       </div>
     </div>
   )
