c34fa5c76c9e702b39ec5204dca06016a4407a20
Haizard <haithammisape@gmail.com>
2023-09-30 05:26:18 -0700

    added some custimiztion to the gallary section to make it look cool

--- src/components/gallery/Gallery.jsx
+++ src/components/gallery/Gallery.jsx
@@ -7,22 +7,16 @@ function Gallery() {
      <div className="gallary_title">
       <span className="gallery_title1">Explore the Beautiful City of Marietta Goergia</span>
       <span className="gallery_content">Discover & connect with great places in and around Cobb Country</span>
-
-      <div className="gallery_pic_section">
-          
-          <div className="gallery_pic_container">
-       
-             <div className="gallary_container1">
-                  <img className='img_pic' src={blog1} alt="" />
-              <div className="content_box2">
-                <span className="text">hi there</span>
-              </div>
-             </div>
+           
+        <div class="grid-container">
+            <div class="grid-item item1"><img className='img_style' src={blog1} alt="" /><span className="garary_content"> Fitness</span></div>
+            <div class="grid-item item2"><img className='img_style' src={blog5} alt="" /><span className="garary_content"> Pizza</span></div>
+            <div class="grid-item item3"><img className='img_style' src={blog9} alt="" /><span className="garary_content"> Wedding</span></div>  
+            <div class="grid-item item4"><img className='img_style' src={blog3} alt="" /><span className="garary_content"> Bike Trail</span></div>
+            <div class="grid-item item5"><img className='img_style' src={blog2} alt="" /><span className="garary_content"> Mental Health</span></div>
+            <div class="grid-item item6"><img className='img_style' src={blog6} alt="" /><span className="garary_content"> Fishing</span></div>
+        </div>
              
-          </div>
-
-      </div>   
-
      </div>
     </div>
   )
