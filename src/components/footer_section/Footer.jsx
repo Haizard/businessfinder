82f31ec0d639547a22a2edfd66da152900d9675b
Haizard <haithammisape@gmail.com>
2023-09-30 06:14:52 -0700

    added some style to the footer

---
+++ src/components/footer_section/Footer.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import './footer.css'
+
+function Footer() {
+  return (
+    <div>
+     <div className="footer_main_section">
+        <div className="website_short_inf">
+           <div className="about_web">
+           <span className="about_web_title">About_web</span>
+
+            <p className="about_p">Close your eyes and transport yourself to an era when the United States was deeply divided, marred by racial segregation, discrimination, and violence. In the midst of this turbulent period, a man named Malcolm Little emerged into the world on May 19, 1925, in Omaha, Nebraska. His life, marked by incredible adversity</p>
+           </div>
+
+           <div className="resources">
+           <span className="resource_title">Resource</span>
+
+            <span className='footer_text'>article and tips</span>
+            <span className='footer_text'>term of service</span>
+           </div>
+
+           <div className="blog_shortcut">
+            <span className="blog_shortcut_title">MariettaBlogArticle</span>
+            <span className='footer_text'>blog</span>
+           </div>
+        </div>
+
+        <div className="description">
+        <p className="about_p">Close your eyes and transport yourself to an era when the United States was deeply divided, marred by racial segregation, discrimination, and violence. In the midst of this turbulent period, a man named Malcolm Little emerged into the world on May 19, 1925, in Omaha, Nebraska. His life, marked by incredible adversity</p>
+
+        </div>
+     </div>
+    </div>
+  )
+}
+
+export default Footer
